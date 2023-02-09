f63760bd7bb002c8aec55b0adba3f5dbaceb1017
unknown <seymen544@gmail.com>
2023-02-09 18:12:57 +0100

    response 6th and 7th completed

--- fib/fib_test.go
+++ fib/fib_test.go
@@ -2,6 +2,7 @@ package fib
 
 import "testing"
 
+/*
 func TestFib(t *testing.T) {
 	input := 1
 	got := Fib(input)
@@ -16,4 +17,31 @@ func TestFib(t *testing.T) {
 	if got != want {
 		t.Errorf("Fib(%d): want %d, got %d", input, want, got)
 	}
+}
+*/
+
+func TestFib(t *testing.T) {
+
+	type test struct {
+		input int
+		want  int
+	}
+
+	var tests = []test{
+		{0, 0},
+		{1, 1},
+		{2, 1},
+		{3, 2},
+
+		// her kan vi bare legge på nye test cases
+	}
+
+	// tc - test case
+	for _, tc := range tests {
+		got := Fib(tc.input)
+		if got != tc.want {
+			t.Errorf("Fib(%d): want %d, got %d", tc.input, tc.want, got)
+		}
+	}
+
 }
